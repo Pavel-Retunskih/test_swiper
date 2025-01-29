d05faec1c1e3d148fac72a194a2bd9ba3c13a86c
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-01-29 23:28:38 +0300

    fix svg

--- src/assets/svg/ExitIcon.tsx
+++ src/assets/svg/ExitIcon.tsx
@@ -1,13 +1,13 @@
 import {Ref, SVGProps, forwardRef, memo} from 'react'
 
 const SvgArrowBackOutline = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
-    <svg ref={ref} {...props} width="23.000000" height="16.000000" viewBox="0 0 23 16" fill="none"
+    <svg ref={ref} {...props} width="30.000000" height="28.000000" viewBox="0 0 30 28" fill="none"
          xmlns="http://www.w3.org/2000/svg"
          xmlnsXlink="http://www.w3.org/1999/xlink">
-
       <defs/>
-      <path id="↓" d="M15.31 0L23 8L15.31 16L13.65 14.54L19.03 9.03L0 9.03L0 6.96L19.03 6.96L13.65 1.45L15.31 0Z"
-            fill="#F3F3F3" fillOpacity="1.000000" fillRule="nonzero"/>
+      <path id="Exclude"
+            d="M9 0L26 0C28.209 0 30 1.79102 30 4L30 24C30 26.209 28.209 28 26 28L9 28C6.79102 28 5 26.209 5 24L5 15.0371L19.0366 15.0371L13.6597 20.5483L15.3188 22L23 14L15.3188 6L13.6597 7.45166L19.0366 12.9629L5 12.9629L5 4C5 1.79102 6.79102 0 9 0ZM5 12.9629L0 12.9629L0 15.0371L5 15.0371L5 12.9629Z"
+            clip-rule="evenodd" fill="#000000" fill-opacity="1.000000" fill-rule="evenodd"/>
     </svg>
 
 )
